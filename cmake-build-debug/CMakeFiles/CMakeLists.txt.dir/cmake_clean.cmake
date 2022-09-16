1b89203ce64d7e279b0f4809f537f67318ff32d2
dszfzzm <dszfzzm@163.com>
2022-09-16 22:27:44 +0800

    删除 *.c.o.d

---
+++ cmake-build-debug/CMakeFiles/CMakeLists.txt.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeLists.txt.dir/function.c.obj"
+  "CMakeFiles/CMakeLists.txt.dir/function.c.obj.d"
+  "CMakeLists.txt.exe"
+  "CMakeLists.txt.exe.manifest"
+  "CMakeLists.txt.pdb"
+  "libCMakeLists.txt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CMakeLists.txt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
