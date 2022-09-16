845975a6480c0649781da1ca8c37eb1a3ff58854
dszfzzm <dszfzzm@163.com>
2022-09-16 23:34:32 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	cmake-build-debug/CMakeFiles/CMakeLists.txt.dir/DependInfo.cmake
    #	cmake-build-debug/CMakeFiles/CMakeLists.txt.dir/build.make
    #	cmake-build-debug/CMakeFiles/CMakeLists.txt.dir/cmake_clean.cmake
    #	cmake-build-debug/CMakeFiles/CMakeLists.txt.dir/compiler_depend.make
    #	cmake-build-debug/CMakeFiles/CMakeLists.txt.dir/objects1.rsp
    #	cmake-build-debug/CMakeFiles/Makefile.cmake
    #	cmake-build-debug/Makefile
    #	cmake-build-debug/feibonaqi.cbp

--- chicken.c
+++ chicken.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main (){
-
+    int rooster=0;
+    int hen=0;
+    int chik=0;
+    for(rooster=20;rooster>0;rooster--){
+        for(hen=(100-rooster*5)/3;hen>0;hen--){
+            chik=100-rooster*5-hen*3;
+            printf("rooster:%d,hen:%d,chik:%d\n",rooster,hen,chik*3);
+        }
+    }
     return 0;
 }
